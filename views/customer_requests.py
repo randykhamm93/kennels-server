68a177f93312214da99f8eb6f633e9c3e382b79a
Randy Hamm <randykhamm@gmail.com>
2023-07-13 11:53:57 -0500

    second commit

---
+++ views/customer_requests.py
@@ -0,0 +1,64 @@
+CUSTOMERS = [
+    {
+        "id": 1,
+        "name": "Ryan Tanay"
+    }
+]
+
+def get_all_customers():
+    return CUSTOMERS
+
+# Function with a single parameter
+def get_single_customer(id):
+    # Variable to hold the found customer, if it exists
+    requested_customer = None
+
+    # Iterate the CUSTOMERS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for customer in CUSTOMERS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if customer["id"] == id:
+            requested_customer = customer
+
+    return requested_customer
+
+def create_customer(customer):
+    # Get the id value of the last customer in the list
+    max_id = CUSTOMERS[-1]["id"]
+
+    # Add 1 to whatever that number is
+    new_id = max_id + 1
+
+    # Add an `id` property to the customer dictionary
+    customer["id"] = new_id
+
+    # Add the customer dictionary to the list
+    CUSTOMERS.append(customer)
+
+    # Return the dictionary with `id` property added
+    return customer
+
+def delete_customer(id):
+    # Initial -1 value for customers index, in case one isn't found
+    customers_index = -1
+
+    # Iterate the CUSTOMERS list, but use enumerate() so that you
+    # can access the index value of each item
+    for index, customers in enumerate(CUSTOMERS):
+        if customers["id"] == id:
+            # Found the customers. Store the current index.
+            customers_index = index
+
+    # If the customers was found, use pop(int) to remove it from list
+    if customers_index >= 0:
+        CUSTOMERS.pop(customers_index)
+
+def update_customer(id, new_customer):
+    # Iterate the CUSTOMERS list, but use enumerate() so that
+    # you can access the index value of each item.
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+            # Found the customer. Update the value.
+            CUSTOMERS[index] = new_customer
+            break
